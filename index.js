2f7dbeb6a0c79a1f07edef9c1e30bcceb1eade67
Alexis Tyler <xo@wvvw.me>
2020-04-07 07:59:44 +0930

    Add ts/js overrides

--- index.js
+++ index.js
@@ -1,32 +1,47 @@
+const spaceBeforeFunctionParam = [
+    'error',
+    'never'
+];
+
 const config = {
-  rules: {
-    'object-curly-spacing': [
-      'error',
-      'always'
-    ],
-    '@typescript-eslint/space-before-function-paren': [
-      'error',
-      'never'
-    ],
-    'space-before-function-paren': [
-      'error',
-      'never'
-    ],
-    'unicorn/catch-error-name': [
-      'error',
-      {
-        name: 'error'
-      }
-    ],
-    quotes: [
-      'error',
-      'single',
-      {
-        avoidEscape: true,
-        allowTemplateLiterals: true
-      }
+    rules: {
+        'object-curly-spacing': [
+            'error',
+            'always'
+        ],
+        'unicorn/catch-error-name': [
+            'error',
+            {
+                name: 'error'
+            }
+        ],
+        quotes: [
+            'error',
+            'single',
+            {
+                avoidEscape: true,
+                allowTemplateLiterals: true
+            }
+        ]
+    },
+    overrides: [
+        {
+            files: [
+                '**/*.js'
+            ],
+            rules: {
+                'space-before-function-paren': spaceBeforeFunctionParam,
+            }
+        },
+        {
+            files: [
+                '**/*.ts'
+            ],
+            rules: {
+                '@typescript-eslint/space-before-function-paren': spaceBeforeFunctionParam
+            }
+        }
     ]
-  }
 };
 
 module.exports = config;
